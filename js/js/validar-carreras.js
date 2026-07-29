2b1e13e3ae9a08a23a6fbbd969af6e6d2bffbaf4
chriszuniga <czunigar@ucenfotec.ac.cr>
2026-07-28 21:33:38 -0600

    Capturar formulario y elementos

---
+++ js/js/validar-carreras.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const formulario = document.getElementById("formCarrera");
+
+    const nombre = document.getElementById("nombreCarrera");
+    const codigo = document.getElementById("codigoCarrera");
+    const grado = document.getElementById("gradoAcademico");
+    const creditos = document.getElementById("creditosCarrera");
+
+    formulario.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+    });
+
+});
