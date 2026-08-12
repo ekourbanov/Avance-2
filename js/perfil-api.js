edd3764a789147a0884360e646c313df28c876d5
chriszuniga <czunigar@ucenfotec.ac.cr>
2026-08-11 23:06:52 -0600

    Preparar mensajes de validacion del perfil

--- js/perfil-api.js
+++ js/perfil-api.js
@@ -5,6 +5,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const formulario =
         document.getElementById("formPerfil");
 
+
+    // Campos obligatorios
+
+    const identificacion =
+        document.getElementById("identificacion");
+
+    const nombreCompleto =
+        document.getElementById("nombreCompleto");
+
+    const correoElectronico =
+        document.getElementById("correoElectronico");
+
+    const telefono =
+        document.getElementById("telefono");
+
+    const fechaRegistro =
+        document.getElementById("fechaRegistro");
+
+
+    // Mensajes de error
+
+    const errorIdentificacion =
+        document.getElementById("errorIdentificacion");
+
+    const errorNombreCompleto =
+        document.getElementById("errorNombreCompleto");
+
+    const errorCorreoElectronico =
+        document.getElementById("errorCorreoElectronico");
+
+    const errorTelefono =
+        document.getElementById("errorTelefono");
+
+    const errorFechaRegistro =
+        document.getElementById("errorFechaRegistro");
+
+
     console.log(
         "Módulo de Perfil cargado. Endpoint:",
         API_URL
